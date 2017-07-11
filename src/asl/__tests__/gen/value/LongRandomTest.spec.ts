1d85f8daf8d10ed5cfcc4535fe2ebf5404ee0eec
Thiago <thiago-dp@bol.com.br>
2017-07-11 03:22:16 -0300

    LongRandomTest modified

--- src/asl/__tests__/gen/value/LongRandomTest.spec.ts
+++ src/asl/__tests__/gen/value/LongRandomTest.spec.ts
@@ -4,11 +4,23 @@ describe( 'LongRandom Test', () => {
 
     let longRandom: LongRandom = new LongRandom();
 
-    it( 'generates a random value between min and max', () => {
+    it( 'generates a random value between min and max, inclusive', () => {
         const min = -2, max = 2;
         let val: number = longRandom.between( min, max );
         expect( val ).toBeGreaterThanOrEqual( min );
         expect( val ).toBeLessThanOrEqual( max );
     } );
+
+    it( 'generates a value greater than a min value', () => {
+        const min = -2;
+        let val: number = longRandom.after( min );
+        expect( val ).toBeGreaterThan( min );
+    } );
+
+    it( 'generates a value lesser than a max value', () => {
+        const max = 2;
+        let val: number = longRandom.before( max );
+        expect( val ).toBeLessThan( max );
+    } );    
 
 } );
