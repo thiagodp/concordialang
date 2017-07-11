939707f7dfbd0aed3276368e93ce9d632a596cd6
Thiago <thiago-dp@bol.com.br>
2017-07-11 04:28:44 -0300

    LongValueGenerator had code comments improved.

--- src/asl/modules/gen/value/LongValueGenerator.ts
+++ src/asl/modules/gen/value/LongValueGenerator.ts
@@ -18,54 +18,93 @@ export class LongValueGenerator extends DefaultValueGenerator< number > {
         this._random = new LongRandom();
     }
 
+    /**
+     * @inheritDoc
+     */
     public hasAvailableValuesOutOfTheRange(): boolean {
 		return this._min > Limits.MIN_INT || this._max < Limits.MAX_INT;		
 	}
 
+    /**
+     * @inheritDoc
+     */
 	public min(): number {
 		return this._min;
 	}
 
+    /**
+     * @inheritDoc
+     */
 	public max(): number {
 		return this._max;
 	}
 
+    /**
+     * @inheritDoc
+     */
 	protected zero(): number {
 		return ( 0 >= this._min && 0 <= this._max ) ? 0 : this._min;
 	}
 
+    /**
+     * @inheritDoc
+     */
 	protected middle(): number {
 		return this._min + ( ( this._max - this._min ) / 2 );
 	}
 	
+    /**
+     * @inheritDoc
+     */    
 	protected hasNext( value: number ): boolean {	
 		return value < this._max;
 	}	
 
+    /**
+     * @inheritDoc
+     */
 	protected next( value: number ): number {
 		return value + 1;
 	}
 	
+    /**
+     * @inheritDoc
+     */    
 	protected hasPrior( value: number ): boolean {
 		return this._min < value;
 	}
 
+    /**
+     * @inheritDoc
+     */
 	protected prior( value: number ): number {
 		return value - 1;
 	}
 
+    /**
+     * @inheritDoc
+     */
 	protected randomBefore( value: number ): number {
 		return this._random.before( value );
 	}
 
+    /**
+     * @inheritDoc
+     */
 	protected randomAfter( value: number ): number {
 		return this._random.after( value );
 	}
 	
+    /**
+     * @inheritDoc
+     */    
 	protected hasAnyValueBetween( min: number, max: number ): boolean {
 		return ( max - min ) > 0;
 	}
 
+    /**
+     * @inheritDoc
+     */
 	protected randomBetween( min: number, max: number ): number {
 		return this._random.between( min, max );
 	}    
