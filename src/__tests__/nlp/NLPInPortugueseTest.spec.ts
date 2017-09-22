d47d04cace9a09e16cae946eb9365ca8f328c9cc
Thiago <thiago-dp@bol.com.br>
2017-09-22 04:46:32 -0300

    NLPTest refactored. NLPTestInPortuguese created.

---
+++ src/__tests__/nlp/NLPInPortugueseTest.spec.ts
@@ -0,0 +1,54 @@
+import { NLPTrainingDataConversor } from '../../modules/nlp/NLPTrainingDataConversor';
+import { NLPEntityUsageExample, NLPTrainingData } from '../../modules/nlp/NLPTrainingData';
+import { NLP } from '../../modules/nlp/NLP';
+
+/**
+ * @author Thiago Delgado Pinto
+ */
+describe( 'NLPInPortugueseTest', () => {
+
+    let nlp: NLP; // under test
+
+    let conversor: NLPTrainingDataConversor = new NLPTrainingDataConversor();
+    let translationMap = makeTranslationMap();
+    let examples = makeTrainingExamples();
+    let data: NLPTrainingData = conversor.convert( translationMap, examples );
+
+    function makeTranslationMap(): any {
+        return require( '../../data/nlp/pt.json' );
+    }
+
+    function makeTrainingExamples(): NLPEntityUsageExample[] {
+        return require( '../../data/training/pt.json' );
+    }
+
+    beforeAll( () => { // once
+        nlp = new NLP();
+        nlp.train( data );
+    } );
+
+    function shouldHaveEntities( results: any[], expectedEntitiesNames: string[] ) {
+        for ( let r of results ) {
+            //console.log( r );        
+            expect( r ).not.toBeFalsy();
+            expect( r.intent ).toEqual( "testcase" );
+            expect( r.entities ).toHaveLength( expectedEntitiesNames.length );
+            let entities = r.entities.map( e => e.entity );
+            expect( entities ).toEqual( expectedEntitiesNames );
+        }
+    }
+
+    it( 'pt - recognizes a click with a value', () => {
+        let results = [];
+        results.push( nlp.recognize( 'eu clico em "x"' ) );
+        results.push( nlp.recognize( 'eu clico na opção "x"' ) );
+        shouldHaveEntities( results, [ "ui_action", "value" ] );
+    } );
+
+    it( 'pt - recognizes a click with a target and a value', () => {
+        let results = [];
+        results.push( nlp.recognize( 'eu clico no botão "x"' ) );
+        shouldHaveEntities( results, [ "ui_action", "ui_target_type", "value" ] );
+    } );    
+
+} );
--- src/__tests__/nlp/NLPTest.spec.ts
+++ src/__tests__/nlp/NLPTest.spec.ts
@@ -9,19 +9,6 @@ describe( 'NLPTest', () => {
 
     let nlp: NLP; // under test
 
-    let conversor: NLPTrainingDataConversor = new NLPTrainingDataConversor();
-    let translationMap = makeTranslationMap();
-    let examples = makeTrainingExamples();
-    let data: NLPTrainingData = conversor.convert( translationMap, examples );
-
-    function makeTranslationMap(): any {
-        return require( '../../data/nlp/pt.json' );
-    }
-
-    function makeTrainingExamples(): NLPEntityUsageExample[] {
-        return require( '../../data/training/pt.json' );
-    }
-
     beforeEach( () => {
         nlp = new NLP();
     } );
@@ -31,34 +18,10 @@ describe( 'NLPTest', () => {
     } );
 
     it( 'can be trained', () => {
+        let conversor: NLPTrainingDataConversor = new NLPTrainingDataConversor();
+        let data: NLPTrainingData = conversor.convert( {}, [] );        
         nlp.train( data );
         expect( nlp.isTrained() ).toBeTruthy();
     } );
-
-    function shouldHaveEntities( results: any[], expectedEntitiesNames: string[] ) {
-        for ( let r of results ) {
-            //console.log( r );        
-            expect( r ).not.toBeFalsy();
-            expect( r.intent ).toEqual( "testcase" );
-            expect( r.entities ).toHaveLength( expectedEntitiesNames.length );
-            let entities = r.entities.map( e => e.entity );
-            expect( entities ).toEqual( expectedEntitiesNames );
-        }
-    }
-
-    it( 'pt - recognizes a click with a value', () => {
-        nlp.train( data );
-        let results = [];
-        results.push( nlp.recognize( 'eu clico em "x"' ) );
-        results.push( nlp.recognize( 'eu clico na opção "x"' ) );
-        shouldHaveEntities( results, [ "ui_action", "value" ] );
-    } );
-
-    it( 'pt - recognizes a click with a target and a value', () => {
-        nlp.train( data );
-        let results = [];
-        results.push( nlp.recognize( 'eu clico no botão "x"' ) );
-        shouldHaveEntities( results, [ "ui_action", "ui_target_type", "value" ] );
-    } );    
 
 } );
