376f08a49840840e47a97820d451c206ff2d17a3
Thiago <thiago-dp@bol.com.br>
2017-10-18 15:03:35 -0200

    ImportLexerTest added.

---
+++ src/__tests__/lexer/ImportLexerTest.spec.ts
@@ -0,0 +1,36 @@
+import { ImportLexer } from '../../modules/lexer/ImportLexer';
+
+/**
+ * @author Thiago Delgado Pinto
+ */
+describe( 'ImportLexerTest', () => {
+
+    // IMPORTANT: since ImportLexer inherits from QuotedNodeLexer and does not add any
+    // behavior, the QuotedNodeLexerTest already covers most test cases.
+
+    let keyword = 'import';
+    let wordInsensitive = 'ImPorT';
+    let word = 'import';
+    let words = [ word ];    
+
+    let lexer = new ImportLexer( words ); // under test
+
+    it( 'ignores a comment after the value', () => {
+        let line = "  \t \t" + word + " \t " + '"Hello world"#comment';
+        
+        let r = lexer.analyze( line, 1 );
+        expect( r ).toBeDefined();
+        expect( r.errors ).toHaveLength( 0 );
+        expect( r.nodes ).toHaveLength( 1 );
+        
+        let node = r.nodes[ 0 ];
+        expect( node ).toEqual(
+            {
+                nodeType: keyword,
+                location: { line: 1, column: 6 },
+                content: "Hello world"                
+            }
+        );
+    } );     
+
+} );
