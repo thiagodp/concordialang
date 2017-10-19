e4f3c4dc6530edf9ff9e890d045820d092f8055d
Thiago <thiago-dp@bol.com.br>
2017-10-19 17:35:12 -0200

    New tests added to ParserTest.

--- src/__tests__/parser/ParserTest.spec.ts
+++ src/__tests__/parser/ParserTest.spec.ts
@@ -200,7 +200,7 @@ describe( 'ParserTest', () => {
     } );
 
     
-    it( 'detects a database and its properties', () => {
+    it( 'detects a database with its properties', () => {
         [
             '#language:en',
             '',
@@ -227,7 +227,7 @@ describe( 'ParserTest', () => {
     } );
 
 
-    it( 'detects a ui element and its properties', () => {
+    it( 'detects a ui element with its properties', () => {
         [
             '#language:en',
             '',
@@ -258,7 +258,7 @@ describe( 'ParserTest', () => {
     } );
 
 
-    it( 'detects a global ui element and its properties', () => {
+    it( 'detects a global ui element with its properties', () => {
         [
             '#language:en',
             '',
@@ -289,6 +289,52 @@ describe( 'ParserTest', () => {
         expect( uie.items[ 1 ].otherwiseSentences ).toHaveLength( 2 );
         expect( uie.items[ 1 ].otherwiseSentences[ 0 ].nodeType == NodeTypes.STEP_OTHERWISE );
         expect( uie.items[ 1 ].otherwiseSentences[ 1 ].nodeType == NodeTypes.STEP_AND );
+    } );
+
+
+    it( 'detects a database with its properties, and a ui element with is properties', () => {
+        [
+            '#language:en',
+            '',
+            'Feature: foo',
+            '',            
+            'Database: My DB',
+            '  - type is "mysql"',
+            '',            
+            '@global',
+            'UI Element: Full Name',
+            '  - id is "#fullname"',
+            '',
+            'UI Element: Birth Date',
+            '  - id is "#birth"',            
+
+        ].forEach( ( val, index ) => lexer.addNodeFromLine( val, index + 1 ) );
+
+        let doc: Document = {};
+        parser.analyze( lexer.nodes(), doc );
+
+        expect( parser.errors() ).toEqual( [] );
+
+        // Database
+        expect( doc.databases ).toHaveLength( 1 );
+        let db = doc.databases[ 0 ];
+        expect( db.name ).toBe( 'My DB' );
+        expect( db.items ).toHaveLength( 1 );
+        expect( db.items[ 0 ].nodeType ).toBe( NodeTypes.DATABASE_PROPERTY );
+
+        // Global UI Element
+        expect( doc.uiElements ).toHaveLength( 1 );        
+        let uie = doc.uiElements[ 0 ];
+        expect( uie.name ).toBe( 'Full Name' );
+        expect( uie.items ).toHaveLength( 1 );
+        expect( uie.items[ 0 ].nodeType ).toBe( NodeTypes.UI_PROPERTY );
+
+        // Local UI Element
+        expect( doc.feature.uiElements ).toHaveLength( 1 );
+        let featureUIE = doc.feature.uiElements[ 0 ];
+        expect( featureUIE.name ).toBe( 'Birth Date' );
+        expect( featureUIE.items ).toHaveLength( 1 );
+        expect( featureUIE.items[ 0 ].nodeType ).toBe( NodeTypes.UI_PROPERTY );
     } );    
 
 } );
