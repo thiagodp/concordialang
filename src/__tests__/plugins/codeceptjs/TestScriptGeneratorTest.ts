e5fee46e3900475dd8ed34a17ed6739de71a1280
Thiago <thiago-dp@bol.com.br>
2017-10-27 21:52:22 -0200

    Test added to TestScriptGeneratorTest.

--- src/__tests__/plugins/codeceptjs/TestScriptGeneratorTest.ts
+++ src/__tests__/plugins/codeceptjs/TestScriptGeneratorTest.ts
@@ -19,7 +19,7 @@ describe( 'TestScriptGeneratorTest', () => {
         gen = new TestScriptGenerator();
     });
 
-    it( 'should generate code for a simple feature without testcases', () => {
+    it( 'generates code for a feature without scenarios and testcases', () => {
 
         let testCase = {
             "schemaVersion": "1.0",
@@ -41,7 +41,7 @@ describe( 'TestScriptGeneratorTest', () => {
     } );
     
 
-    it( 'should generate code for test cases of two different scenarios, even without commands', () => {
+    it( 'generates code for test cases of two different scenarios, even without commands', () => {
         
         let testCase = {
             "schemaVersion": "1.0",
@@ -88,7 +88,7 @@ describe( 'TestScriptGeneratorTest', () => {
     } );
 
 
-    it( 'should generate code for one test case with commands', () => {
+    it( 'generates code for a single test case', () => {
 
         let testCase = {
             "schemaVersion": "1.0",
@@ -107,7 +107,7 @@ describe( 'TestScriptGeneratorTest', () => {
                 {
                     "location": { "column": 1, "line": 40 },
                     "scenario": "successful login",
-                    "name": "finishes successfully valid values",
+                    "name": "finishes successfully with valid values",
         
                     "commands": [
                         {
@@ -121,23 +121,23 @@ describe( 'TestScriptGeneratorTest', () => {
                             "action": "fill",
                             "targets": [ "#username" ],
                             "targetType": "textbox",
-                            "values": [ "bob" ],
+                            "values": [ "bob" ]
                         },
                         {
                             "location": { "column": 1, "line": 43 },
                             "action": "fill",
                             "targets": [ "#password" ],
                             "targetType": "textbox",
-                            "values": [ "b0bp4s$" ],
-                        },                        
+                            "values": [ "b0bp4s$" ]
+                        },
                         {
                             "location": { "column": 1, "line": 43 },
                             "action": "click",
                             "targets": [ "#enter" ],
                             "targetType": "button"
-                        }                
+                        }
                     ]
-                }        
+                }
             ]
         } as AbstractTestScript;
 
@@ -146,7 +146,7 @@ describe( 'TestScriptGeneratorTest', () => {
             
             Feature('login');
 
-            Scenario('successful login | finishes successfully valid values', (I) => {
+            Scenario('successful login | finishes successfully with valid values', (I) => {
                 I.see('Login');
                 I.fillField('#username', 'bob');
                 I.fillField('#password', 'b0bp4s$');
@@ -156,5 +156,121 @@ describe( 'TestScriptGeneratorTest', () => {
 
         expect( uglify( gen.generate( testCase ) ) ).toBe( uglify( expected ) );
     } );
+
+
+    it( 'generates code for more than one test case', () => {
+
+        let testCase = {
+            "schemaVersion": "1.0",
+            "sourceFile": "path/to/somefile.testcase",
+            "feature": {
+                "location": { "column": 1, "line": 1 },
+                "name": "login"
+            },
+            "scenarios": [
+                {
+                    "location": { "column": 1, "line": 3 },
+                    "name": "successful login"
+                }
+            ],  
+            "testcases": [
+                {
+                    "location": { "column": 1, "line": 40 },
+                    "scenario": "successful login",
+                    "name": "finishes successfully with valid values",
+        
+                    "commands": [
+                        {
+                            "location": { "column": 1, "line": 41 },
+                            "action": "see",
+                            "targets": [ "Login" ],
+                            "targetType": "text"
+                        },
+                        {
+                            "location": { "column": 1, "line": 42 },
+                            "action": "fill",
+                            "targets": [ "#username" ],
+                            "targetType": "textbox",
+                            "values": [ "bob" ]
+                        },
+                        {
+                            "location": { "column": 1, "line": 43 },
+                            "action": "fill",
+                            "targets": [ "#password" ],
+                            "targetType": "textbox",
+                            "values": [ "b0bp4s$" ]
+                        },
+                        {
+                            "location": { "column": 1, "line": 43 },
+                            "action": "click",
+                            "targets": [ "#enter" ],
+                            "targetType": "button"
+                        }
+                    ]
+                },
+
+
+                {
+                    "location": { "column": 1, "line": 40 },
+                    "scenario": "unsuccessful login",
+                    "name": "finishes unsuccessfully with invalid values",
+                    "invalid": true,
+        
+                    "commands": [
+                        {
+                            "location": { "column": 1, "line": 41 },
+                            "action": "see",
+                            "targets": [ "Login" ],
+                            "targetType": "text"
+                        },
+                        {
+                            "location": { "column": 1, "line": 42 },
+                            "action": "fill",
+                            "targets": [ "#username" ],
+                            "targetType": "textbox",
+                            "values": [ "kdsldhçs dwd" ],
+                            "invalid": true
+                        },
+                        {
+                            "location": { "column": 1, "line": 43 },
+                            "action": "fill",
+                            "targets": [ "#password" ],
+                            "targetType": "textbox",
+                            "values": [ "d0d s98 23923 2 32$" ],
+                            "invalid": true
+                        },                        
+                        {
+                            "location": { "column": 1, "line": 43 },
+                            "action": "click",
+                            "targets": [ "#enter" ],
+                            "targetType": "button"
+                        }
+                    ]
+                }
+            ]
+        } as AbstractTestScript;
+
+        let expected = `
+            ${comment}
+            
+            Feature('login');
+
+            Scenario('successful login | finishes successfully with valid values', (I) => {
+                I.see('Login');
+                I.fillField('#username', 'bob');
+                I.fillField('#password', 'b0bp4s$');
+                I.click('#enter');
+            });
+
+            Scenario('unsuccessful login | finishes unsuccessfully with invalid values', (I) => {
+                I.see('Login');
+                I.fillField('#username', 'kdsldhçs dwd');
+                I.fillField('#password', 'd0d s98 23923 2 32$');
+                I.click('#enter');
+            });            
+        `;
+
+        expect( uglify( gen.generate( testCase ) ) ).toBe( uglify( expected ) );
+    } );    
     
 } );
