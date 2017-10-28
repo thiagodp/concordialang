aba9057a834b25901f4f80962de9169d38b7a3cf
Thiago <thiago-dp@bol.com.br>
2017-10-27 23:03:06 -0200

    main.ts updated.

--- src/main.ts
+++ src/main.ts
@@ -16,34 +16,34 @@ const cli = meow( `
 
  Options:
 
-  -p, --plugin=<name>                   Sets the plug-in to generate or to execute test scripts. NIY
+  -p, --plugin <name>                   Sets the plug-in to generate or to execute test scripts. NIY
 
-  -x, --extensions=<".ext1,.ext2,...">  File extensions to consider for the given <dir>. NIY
-  -i, --ignore=<"file1,file2,...">      Files to ignore from the given <dir>.
-  -f, --files=<"file1,file2,...">       Files to consider instead of <dir>.
+  -x, --extensions <".ext1,.ext2,...">  File extensions to consider (when <dir> is informed). NIY
+  -i, --ignore <"file1,file2,...">      Files to ignore (when <dir> is informed).
 
-  -l, --language=<code>                 Sets the default language (from "en"glish).
-  -e, --encoding=<value>                Sets the enconding to read files. Default is "utf8".
+  -f, --files <"file1,file2,...">       Files to consider, instead of <dir>.
+
+  -l, --language <code>                 Sets the default language (from "en"glish).
+  -e, --encoding <value>                Sets the enconding to read files. Default is "utf8".
 
   -t, --no-test                         Do not generate test cases. NIY
   -s, --no-script                       Do not generate test scripts. NIY
   -r, --no-run                          Do not run test scripts. NIY
   -u, --no-result                       Do not read execution results. NIY
 
-  --test-connection=<name|*>            Tests the connection with a database or
-                                        file which name was defined in the spec. NIY
+  --test-connection <name|*>            Tests a database connection with the given name. NIY
 
-  --min-fi-gen=<number>                 Min. feature importance to generate test cases. NIY
-  --max-fi-gen=<number>                 Max. feature importance to generate test cases. NIY
-  --min-si-gen=<number>                 Min. scenario importance to generate test cases. NIY
-  --max-si-gen=<number>                 Max. scenario importance to generate test cases. NIY
+  --min-fi-gen <number>                 Min. feature importance to generate test cases. NIY
+  --max-fi-gen <number>                 Max. feature importance to generate test cases. NIY
+  --min-si-gen <number>                 Min. scenario importance to generate test cases. NIY
+  --max-si-gen <number>                 Max. scenario importance to generate test cases. NIY
 
-  --min-fi-run=<number>                 Min. feature importance to run test scripts. NIY
-  --max-fi-run=<number>                 Max. feature importance to run test scripts. NIY  
-  --min-si-run=<number>                 Min. scenario importance to run test scripts. NIY
-  --max-si-run=<number>                 Max. scenario importance to run test scripts. NIY
+  --min-fi-run <number>                 Min. feature importance to run test scripts. NIY
+  --max-fi-run <number>                 Max. feature importance to run test scripts. NIY  
+  --min-si-run <number>                 Min. scenario importance to run test scripts. NIY
+  --max-si-run <number>                 Max. scenario importance to run test scripts. NIY
 
-  --max-random-tests=<0-999>            Max. number of test cases to use random values. NIY
+  --max-random-tests <0-999>            Max. number of test cases to use random values. NIY
 
   -g, --plugin-list                     Show available plug-ins. NIY
   -v, --version                         Show current version.
@@ -52,9 +52,9 @@ const cli = meow( `
 	  
   Examples:
 
-   $ ${exeName} . --plugin=some-plugin
-   $ ${exeName} --files="file1.feature,path/to/file2.feature" -p=some-plugin -l=pt
-   $ ${exeName} path/to/dir --no-test --no-script -p=some-plugin      
+   $ ${exeName} . --plugin some-plugin
+   $ ${exeName} --files "file1.feature,path/to/file2.feature" -p some-plugin -l pt
+   $ ${exeName} path/to/dir --no-test --no-script -p some-plugin
 `, {
 	alias: {
 		p: 'plugin',
