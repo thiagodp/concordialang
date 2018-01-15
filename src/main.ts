bbe883348b13a4c1d0bf73f2266742a4694f07fb
Thiago <thiago-dp@bol.com.br>
2018-01-15 18:21:12 -0200

    Execution options updated (main.ts).

--- src/main.ts
+++ src/main.ts
@@ -7,58 +7,64 @@ import * as util from 'util';
 import { InputProcessor } from './modules/cli/InputProcessor';
 import { PluginInputProcessor } from './modules/testscript/PluginInputProcessor';
 
-const exeName: string = 'concordia';
+const exeName: string = chalk.magenta( 'concordia' );
+const NIY: string = chalk.red( 'NIY' );
 
 const cli = meow( `
- Usage:
+ ${chalk.yellowBright('Usage:')} ${exeName} [ <dir> | --files="file1.feature,path/to/file2.feature,..." ] [options]
 
-  ${exeName} [ <dir> | --files="file1.feature,path/to/file2.feature,..." ] [options]
+    where ${chalk.yellow('<dir>')} is the directory that contains your specification files.
 
-    where <dir> is the directory that contains your specification files.
+ ${chalk.yellowBright('Options:')}
 
- Options:
-
-  -p,  --plugin <name>                   Sets a plug-in to generate or to execute test scripts. NIY
-  -pl, --plugin-list                     Shows available plug-ins. NIY
-  -pa, --plugin-about <name>             Shows more information about a plug-in. NIY
-  -pi, --plugin-install <name>           Installs a plug-in. NIY
-  -pu, --plugin-uninstall <name>         Uninstalls a plug-in. NIY
-
-  -x,  --extensions <".ext1,.ext2,...">  File extensions to consider (when <dir> is informed). NIY
-  -i,  --ignore <"file1,file2,...">      Files to ignore (when <dir> is informed).
-
+  -e,  --encoding <value>                File encoding. Default is "utf8".
+  -x,  --extensions <".ext1,.ext2,...">  File extensions to consider (when <dir> is informed). ${NIY}
+  -i,  --ignore <"file1,file2,...">      Files to ignore, when <dir> is informed.
   -f,  --files <"file1,file2,...">       Files to consider, instead of <dir>.
 
-  -l,  --language <code>                 Sets the default language (from "en"glish).
-  -e,  --encoding <value>                Sets the enconding to read files. Default is "utf8".
+  -l,  --language <code>                 Default language. Default is "en" (english).
+  -ll, --language-list                   List available languages. ${NIY}
 
-  -t,  --no-test                         Do not generate test cases. NIY
-  -s,  --no-script                       Do not generate test scripts. NIY
-  -r,  --no-run                          Do not run test scripts. NIY
-  -u,  --no-result                       Do not read execution results. NIY
+  -p,  --plugin <name>                   Plug-in to use. ${NIY}
+  -pl, --plugin-list                     List available plug-ins.
+  -pa, --plugin-about <name>             About a plug-in.
+  -pi, --plugin-install <name>           Install a plug-in.
+  -pu, --plugin-uninstall <name>         Uninstall a plug-in.  
 
-  -tc, --test-connection <name|*>        Tests a database connection with the given name. NIY
+  -nt, --no-test                         Do not generate test cases. ${NIY}
+  -ns, --no-script                       Do not generate test scripts. ${NIY}
+  -nr, --no-run                          Do not run test scripts. ${NIY}
+  -nu, --no-result                       Do not read execution results. ${NIY}
 
+  -b,  --verbose                         Verbose output. ${NIY}
   -v,  --version                         Show current version.
   -a,  --about                           Show information about this application.
-  -h,  --help                            Show this help.  
+  -h,  --help                            Show this help.
+  -n,  --newer                           Check for newer versions. ${NIY}
 
-  Related to the generation of test cases:
+  ${chalk.gray('Randomic generation options:')}
 
-  --gen-min-feature <number>             Selects only the features with the given minimum importance. NIY
-  --gen-max-feature <number>             Selects only the features with the given maximum importance. NIY
-  --gen-min-scenario <number>            Selects only the scenarios with the given minimum importance. NIY
-  --gen-max-scenario <number>            Selects only the scenarios with the given maximum importance. NIY
-  --gen-max-random-cases <0-999>         Maximum number of test cases that to use random values. NIY
+  --random-seed <number>                 Use the given random seed. ${NIY}
+  --random-valid <0-9999>                Number of test cases with valid random values. ${NIY}
+  --random-invalid <0-9999>              Number of test cases with invalid random values. ${NIY}  
 
-  Related to the execution of test scripts (used plugin may not support them!):
+  ${chalk.gray('Test minimization options:')}
 
-  --run-min-feature <number>             Executes only the features with the given minimum importance. NIY
-  --run-max-feature <number>             Executes only the features with the given maximum importance. NIY
-  --run-min-scenario <number>            Executes only the scenarios with the given minimum importance. NIY
-  --run-max-scenario <number>            Executes only the scenarios with the given maximum importance. NIY
+  --sel-min-feature <number>             Minimum feature importance. ${NIY}
+  --sel-max-feature <number>             Maximum feature importance. ${NIY}
+  --sel-min-scenario <number>            Minimum scenario importance. ${NIY}
+  --sel-max-scenario <number>            Maximum scenario importance. ${NIY}
+  --sel-filter <filter>                  Filter by tag. ${NIY}
+
+  ${chalk.gray('Execution filtering options (depends on the used plugin):')}
+
+  --run-min-feature <number>             Minimum feature importance. ${NIY}
+  --run-max-feature <number>             Maximum feature importance. ${NIY}
+  --run-min-scenario <number>            Minimum scenario importance. ${NIY}
+  --run-max-scenario <number>            Maximum scenario importance. ${NIY}
+  --run-filter <filter>                  Filter by tag. ${NIY}
 	  
-  Examples:
+  ${chalk.yellowBright('Examples:')}  
 
    $ ${exeName} . --plugin some-plugin
    $ ${exeName} path/to/dir --no-test --no-script -p some-plugin   
@@ -75,14 +81,15 @@ const cli = meow( `
 		f: 'files',
 		i: 'ignore',
 		x: 'extensions',
-        t: 'no-test',
-        s: 'no-script',
-		r: 'no-run',
-        u: 'no-result',
+        nt: 'no-test',
+        ns: 'no-script',
+		nr: 'no-run',
+        nu: 'no-result',
         tc: 'test-connection',
         v: 'version',        
         a: 'about',
-        h: 'help'
+        h: 'help',
+        n: 'newer'
 	}
 });
 
