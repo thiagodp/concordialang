227253e9e88ba135f091d2780e091d93287e17a1
Thiago <thiago-dp@bol.com.br>
2018-01-17 15:37:09 -0200

    New QueryBasedDataGenerator

--- src/modules/data-gen/QueryBasedDataGenerator.ts
+++ src/modules/data-gen/QueryBasedDataGenerator.ts
@@ -1,10 +1,13 @@
 import { RawDataGenerator } from "./raw/RawDataGenerator";
 import { RandomLong } from "./random/RandomLong";
-import { DatabaseInterface } from "../db/DatabaseInterface";
 import { QueryCache } from "../db/QueryCache";
+import { Queryable } from "../db/Queryable";
 
 /**
  * Query-based data generator.
+ * 
+ * Known limitations:
+ * - It always generates values from the first column returned by the query.
  * 
  * @author Thiago Delgado Pinto
  */
@@ -15,8 +18,7 @@ export class QueryBasedDataGenerator< T > {
      * 
      * @param _random Random number generator.
      * @param _rawDataGenerator Raw data generator
-     * @param _dbi Database interface
-     * @param _connCache Connection cache
+     * @param _queriable Queriable
      * @param _queryCache Query cache
      * @param _query SQL query 
      * @param _maxTries Max tries to generate an element which does not belong to the set
@@ -24,67 +26,95 @@ export class QueryBasedDataGenerator< T > {
     constructor(
         private _random: RandomLong,
         private _rawDataGenerator: RawDataGenerator< T >,
-        private _dbi: DatabaseInterface,
+        private _queriable: Queryable,
         private _queryCache: QueryCache,
         private _query: string,
         private _maxTries: number = 10
     ) {
-
     }
 
     // DATA GENERATION
 
-    public async firstElement(): Promise< T | null > {
-        return null; // TO-DO
-    }
+    public firstElement = async (): Promise< T | null > => {
+        const values: any[] = await this.queryValues();
+        return values.length > 0
+            ? this.valueOfTheFirstColumn( values[ 0 ] )
+            : null;
+    };
 
-    public async secondElement(): Promise< T | null > {
-        return null; // TO-DO
-    }
+    public secondElement = async (): Promise< T | null > => {
+        const values: any[] = await this.queryValues();
+        return values.length > 1
+            ? this.valueOfTheFirstColumn( values[ 1 ] )
+            : null;
+    };
 
-    public async randomElement(): Promise< T | null > {
-        return null; // TO-DO
-    }
+    public randomElement = async (): Promise< T | null > => {
+        const values: any[] = await this.queryValues();
+        if ( values.length < 1 ) {
+            return null;
+        }
+        const index: number = this._random.between( 0, values.length );
+        return this.valueOfTheFirstColumn( values[ index ] );
+    };
 
-    public async penultimateElement(): Promise< T | null > {
-        return null; // TO-DO
-    }
+    public penultimateElement = async (): Promise< T | null > => {
+        const values: any[] = await this.queryValues();
+        const len: number = values.length;
+        return len > 1
+            ? this.valueOfTheFirstColumn( values[ len - 2 ] )
+            : null;
+    };
 
-    public async lastElement(): Promise< T | null > {
-        return null; // TO-DO
-    }
+    public lastElement = async (): Promise< T | null > => {
+        const values: any[] = await this.queryValues();
+        return values.length > 0
+            ? this.valueOfTheFirstColumn( values[ values.length - 1 ] )
+            : null;
+    };
 
-    public async notInSet(): Promise< T | null > {
+    public notInSet = async (): Promise< T | null > => {
         for ( let i = 0; i < this._maxTries; ++i ) {
-            let val: T = this._rawDataGenerator.randomBetweenMinAndMax();
-            if ( ! this.hasValue( val ) ) {
+            const val: T = this._rawDataGenerator.randomBetweenMinAndMax();
+            const found: boolean = await this.hasValue( val );
+            if ( ! found ) {
                 return val;
             }
         }
         return null;
-    }
+    };
 
     // UTIL
 
-    private async queryValues(): Promise< Map< string, any >[] > {
+    private valueOfTheFirstColumn = ( row: any ): T | null => {
+        if ( ! row ) {
+            return null;
+        }
+        for ( let key in row ) { // works for both an object or an array of values
+            return row[ key ];
+        }
+        return null;
+    };
 
+    private queryValues = async (): Promise< any[] > => {
         if ( this._queryCache.has( this._query ) ) {
             return this._queryCache.get( this._query );
         }
-
-        let result: Map< string, any >[] = [];
-
-        // exec query...        
-        this._dbi.query( this._query )
-
+        const result = await this._queriable.query( this._query );
         this._queryCache.put( this._query, result );
-
         return result;
-    }
+    };
 
 
-    private hasValue( value: T ): boolean {
-        return false; // TO-DO
-    }
+    private hasValue = async ( value: T ): Promise< boolean > => {
+        const values: any[] = await this.queryValues();
+        for ( let row of values ) {
+            const val = this.valueOfTheFirstColumn( row );
+            if ( val == value ) {
+                return true;
+            }
+        }
+        return false;
+    };
 
 }
