2b8aaa831af2e9c81ef17aa69b11f18243b00484
Thiago <thiago-dp@bol.com.br>
2018-01-17 11:05:36 -0200

    StringGeneratorTest added.

---
+++ src/__tests__/data-gen/raw/StringGeneratorTest.spec.ts
@@ -0,0 +1,49 @@
+import { StringGenerator } from "../../../modules/data-gen/raw/StringGenerator";
+import { RandomString } from "../../../modules/data-gen/random/RandomString";
+import { Random } from "../../../modules/data-gen/random/Random";
+
+describe( 'StringGeneratorTest', () => {
+
+    const ranL = new RandomString( new Random() );
+    const aMin: number = 2;
+    const aMax: number = 10;
+    const aMedian: number = 6;
+    const gen: StringGenerator = new StringGenerator( ranL, aMin, aMax );
+
+    it( 'random below min', () => {
+        expect( gen.randomBelowMin().length ).toBeLessThan( aMin );
+    } );    
+
+    it( 'just below min', () => {
+        expect( gen.justBelowMin().length ).toBe( aMin - 1 );
+    } );
+    
+    it( 'just above min', () => {
+        expect( gen.justAboveMin().length ).toBe( aMin + 1 );
+    } );    
+
+    it( 'median value', () => {
+        expect( gen.median().length ).toBe( aMedian );
+        const gen2: StringGenerator = new StringGenerator( ranL, 1, 1 );
+        expect( gen2.median().length ).toBe( 1 );
+    } );
+    
+    it( 'random between min and max', () => {
+        const val = gen.randomBetweenMinAndMax().length;
+        expect( val ).toBeGreaterThan( aMin );
+        expect( val ).toBeLessThan( aMax );
+    } );
+
+    it( 'just below max', () => {
+        expect( gen.justBelowMax().length ).toBe( aMax - 1 );
+    } );
+
+    it( 'just above max', () => {
+        expect( gen.justAboveMax().length ).toBe( aMax + 1 );
+    } );
+
+    it( 'random above max', () => {
+        expect( gen.randomAboveMax().length ).toBeGreaterThan( aMax );
+    } );
+    
+} );
