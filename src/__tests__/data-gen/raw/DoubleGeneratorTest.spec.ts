5ee7bc61174abfe890999ed07e12acbffaf23471
Thiago <thiago-dp@bol.com.br>
2018-01-17 11:00:57 -0200

    DoubleGeneratorTest has new test added.

--- src/__tests__/data-gen/raw/DoubleGeneratorTest.spec.ts
+++ src/__tests__/data-gen/raw/DoubleGeneratorTest.spec.ts
@@ -6,9 +6,20 @@ describe( 'DoubleGeneratorTest', () => {
 
     let ranD = new RandomDouble( new Random() );
 
+    it( 'random below min', () => {
+        const min = '0.00';
+        const gen: DoubleGenerator = new DoubleGenerator( ranD, min, '1.00' );
+        expect( gen.randomBelowMin() ).toBeLessThan( Number( min ) );
+    } );    
+
     it( 'just below min', () => {
         const gen: DoubleGenerator = new DoubleGenerator( ranD, '0.00', '1.00' );
         expect( gen.justBelowMin() ).toBeCloseTo( -0.01, 10 );
+    } );
+    
+    it( 'just above min', () => {
+        const gen: DoubleGenerator = new DoubleGenerator( ranD, '0.00', '1.00' );
+        expect( gen.justAboveMin() ).toBeCloseTo( 0.01, 10 );
     } );    
 
     it( 'median value', () => {
@@ -19,5 +30,29 @@ describe( 'DoubleGeneratorTest', () => {
         expect( gen2.median() ).toBeCloseTo( 1, 10 );
     } );
     
+    it( 'random between min and max', () => {
+        const min = '0.0';
+        const max = '1.0';
+        const gen: DoubleGenerator = new DoubleGenerator( ranD, min, max );
+        const val = gen.randomBetweenMinAndMax();
+        expect( val ).toBeGreaterThan( Number( min ) );
+        expect( val ).toBeLessThan( Number( max ) );
+    } );
 
+    it( 'just below max', () => {
+        const gen: DoubleGenerator = new DoubleGenerator( ranD, '0.00', '1.00' );
+        expect( gen.justBelowMax() ).toBeCloseTo( 0.99, 10 );
+    } );
+
+    it( 'just above max', () => {
+        const gen: DoubleGenerator = new DoubleGenerator( ranD, '0.00', '1.00' );
+        expect( gen.justAboveMax() ).toBeCloseTo( 1.01, 10 );
+    } );
+
+    it( 'random above max', () => {
+        const max = '1.00';
+        const gen: DoubleGenerator = new DoubleGenerator( ranD, '0.00', max );
+        expect( gen.randomAboveMax() ).toBeGreaterThan( Number( max ) );
+    } );
+    
 } );
