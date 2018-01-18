d2ad1cb62b9d5dc060832d01e8ce0056ad19c52b
Thiago <thiago-dp@bol.com.br>
2018-01-18 19:01:49 -0200

    PluginInputProcessor slightly refactored.

--- src/modules/testscript/PluginInputProcessor.ts
+++ src/modules/testscript/PluginInputProcessor.ts
@@ -20,7 +20,7 @@ export class PluginInputProcessor {
             .then( ( plugins: TestScriptPluginData[] ) => {
                 this.drawPluginList( plugins );
             } )
-            .catch( ( err ) => this._write( err.message ) );
+            .catch( ( err ) => this.write( err.message ) );
     };
 
     about = ( name: string ): void => {
@@ -33,7 +33,7 @@ export class PluginInputProcessor {
                 }
                 this.drawSinglePlugin( found[ 0 ] );
             } )
-            .catch( ( err ) => this._write( err.message ) );
+            .catch( ( err ) => this.write( err.message ) );
     };
 
     install = ( name: string ): void => {
@@ -47,13 +47,13 @@ export class PluginInputProcessor {
                 }
                 const command = found[ 0 ].install;
                 if ( ! command ) {
-                    this._write( 'No "install" property found in the plugin file. Can\'t install it.' );
+                    this.write( 'No "install" property found in the plugin file. Can\'t install it.' );
                     return;
                 }
-                this._write( 'Installing the plugin ' + chalk.yellow( name ) + '...' );
+                this.write( 'Installing the plugin ' + chalk.yellow( name ) + '...' );
                 this.runPluginCommand( command );
             } )
-            .catch( ( err ) => this._write( err.message ) );
+            .catch( ( err ) => this.write( err.message ) );
     };
 
     uninstall = ( name: string ): void => {
@@ -67,13 +67,13 @@ export class PluginInputProcessor {
                 }
                 const command = found[ 0 ].uninstall;
                 if ( ! command ) {
-                    this._write( 'No "uninstall" property found in the plugin file. Can\'t uninstall it.' );
+                    this.write( 'No "uninstall" property found in the plugin file. Can\'t uninstall it.' );
                     return;
                 }
-                this._write( 'Uninstalling the plugin ' + chalk.yellow( name ) + '...' );
+                this.write( 'Uninstalling the plugin ' + chalk.yellow( name ) + '...' );
                 this.runPluginCommand( command );
             } )
-            .catch( ( err ) => this._write( err.message ) );        
+            .catch( ( err ) => this.write( err.message ) );        
     };
 
     private sortPlugins = ( plugins: TestScriptPluginData[] ): TestScriptPluginData[] => {
@@ -84,28 +84,28 @@ export class PluginInputProcessor {
 
     private drawPluginList = ( plugins: TestScriptPluginData[] ): void => {
         const format = "%-20s %-8s %-22s"; // util.format does not support padding :(
-        this._write( chalk.yellow( sprintf( format, 'Name', 'Version', 'Description' ) ) );
+        this.write( chalk.yellow( sprintf( format, 'Name', 'Version', 'Description' ) ) );
         plugins = this.sortPlugins( plugins );
         for ( let p of plugins ) {
-            this._write( sprintf( format, p.name, p.version, p.description ) );
+            this.write( sprintf( format, p.name, p.version, p.description ) );
         }
     };
 
     private drawSinglePlugin = ( p: TestScriptPluginData ): void => {
         const format = "  - %-12s: %s"; // util.format does not support padding :(
-        this._write( 'Plugin ' + chalk.yellow( p.name ) );
-        this._write( sprintf( format, 'version', p.version ) );
-        this._write( sprintf( format, 'description', p.description ) );
-        this._write( sprintf( format, 'targets', p.targets.join( ', ' ) ) );
+        this.write( 'Plugin ' + chalk.yellow( p.name ) );
+        this.write( sprintf( format, 'version', p.version ) );
+        this.write( sprintf( format, 'description', p.description ) );
+        this.write( sprintf( format, 'targets', p.targets.join( ', ' ) ) );
         const authors = p.authors.map( ( a, idx ) => 0 === idx ? a : sprintf( '%-17s %s', '', a ) );
-        this._write( sprintf( format, 'authors', authors.join( '\n') ) );        
-        this._write( sprintf( format, 'fake', p.isFake ? 'yes': 'no' ) );        
-        this._write( sprintf( format, 'file', p.file ) );
-        this._write( sprintf( format, 'class', p.class ) );        
+        this.write( sprintf( format, 'authors', authors.join( '\n') ) );        
+        this.write( sprintf( format, 'fake', p.isFake ? 'yes': 'no' ) );        
+        this.write( sprintf( format, 'file', p.file ) );
+        this.write( sprintf( format, 'class', p.class ) );        
     };
 
     private showMessageOnNoPluginFound = ( name: string ): void => {
-        this._write( 'No plugins found with the name "' + chalk.yellow( name ) + '".'
+        this.write( 'No plugins found with the name "' + chalk.yellow( name ) + '".'
             + '\nTry ' + chalk.yellow( '--plugin-list' ) + ' to see the available plugins.' );
     };
 
@@ -116,21 +116,25 @@ export class PluginInputProcessor {
     };
 
     private runPluginCommand = ( command: string ): void => {
-        this._write( '---' );        
+        this.write( '---' );        
         const child = childProcess.exec( command );
         
         child.stdout.on( 'data', ( chunk ) => {
-            this._write( chunk.toString() );
+            this.write( chunk.toString() );
         } );
 
         child.stderr.on( 'data', ( chunk ) => {
-            this._write( chunk.toString() );
+            this.write( chunk.toString() );
         } );
 
         child.on( 'exit', ( code ) => {
-            this._write( '---' );
-            this._write( ( 0 == code ? chalk.bgGreen( 'Success' ) : chalk.bgRed( 'Errors were found.' ) ) );
+            this.write( '---' );
+            this.write( ( 0 == code ? chalk.bgGreen( 'Success' ) : chalk.bgRed( 'Errors were found.' ) ) );
         } );                
+    };
+
+    private write = ( ... args ): void => {
+        this._write( ... args );
     };
 
 }
