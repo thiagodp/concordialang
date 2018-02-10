41b7951371494f3ba2be2f656f19ffe1ee96bd21
Thiago <thiago-dp@bol.com.br>
2018-02-10 05:29:35 -0200

    copy.js changed to copy JSON files from data/ and plugins/

--- copy.js
+++ copy.js
@@ -1,6 +1,17 @@
-const fs = require( 'fs-extra' );
+var glob = require( 'glob' );
+var copyFile = require( 'fs' ).copyFile;
+var basename = require( 'path' ).basename;
 
-fs.copy( './data', './dist/data', err => {
-    if ( err ) return console.error( err );
-    console.log( '> data folder copied successfully!' );
+// Copy JSON files from the following folders to 'dist/'
+[ 'data/', 'plugins/' ].forEach( function( dir ) {
+    glob( dir + '*.json', function( globErr, matches ) {
+        if ( globErr ) return console.error( globErr );
+        matches.forEach( function( from ) {
+            var to = 'dist/' + dir + basename( from );
+            copyFile( from, to, function( copyErr ) {
+                if ( copyErr ) return console.error( copyErr );
+                console.log( '> copying', from, 'to', to );
+            } );
+        } );
+    } );
 } );
