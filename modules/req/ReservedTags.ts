61f48563d80171b68d7ca293f8dc6f86fd36705e
Thiago <thiago-dp@bol.com.br>
2018-03-05 17:43:20 -0300

    Added 'importance' to the reserved tags.

--- modules/req/ReservedTags.ts
+++ modules/req/ReservedTags.ts
@@ -5,13 +5,14 @@
  */
 export enum ReservedTags {
 
-    GENERATED = 'generated', // example: @generated
+    GENERATED = 'generated', //     example: @generated
     
-    FEATURE = 'feature', // example: @feature( login )
-    SCENARIO = 'scenario', // example: @scenario( sucessful login )
-    TEMPLATE = 'template', // example: @template( successful login )
+    FEATURE = 'feature', //         example: @feature( login )
+    SCENARIO = 'scenario', //       example: @scenario( sucessful login )
+    TEMPLATE = 'template', //       example: @template( successful login )
 
-    GLOBAL = 'global', // example: @global
-    IGNORE = 'ignore' // example: @ignore
+    GLOBAL = 'global', //           example: @global
+    IGNORE = 'ignore', //           example: @ignore
+    IMPORTANCE = 'importance' //    example: @importance( 5 )
 
 }
