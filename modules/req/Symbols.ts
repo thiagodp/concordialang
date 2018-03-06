3a06fd6e7b5f9682ed54726642a7cf63a0284e65
Thiago <thiago-dp@bol.com.br>
2018-03-06 19:15:05 -0300

    New symbols added.

--- modules/req/Symbols.ts
+++ modules/req/Symbols.ts
@@ -13,6 +13,14 @@ export abstract class Symbols {
     static PY_STRING_PREFIX = '"""';
     static TABLE_PREFIX = '|';
     static LIST_ITEM_PREFIX = '-';
+    static UI_ELEMENT_PREFIX = '{';
+    static UI_LITERAL_PREFIX = '<';
+    static CONSTANT_PREFIX = '[';
+
+    // sufixes
+    static UI_LITERAL_SUFFIX = '>';
+    static UI_ELEMENT_SUFFIX = '}';
+    static CONSTANT_SUFFIX = ']';
 
     // separators
 
