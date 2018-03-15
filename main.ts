f6682d0b5a369f20bc5426bf89db014f9e629d5f
Thiago <thiago-dp@bol.com.br>
2018-03-15 07:32:51 -0300

    Changes main to define behavior on uncaughtException

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import { AppController } from "./modules/app/AppController";
 
+process.on( 'uncaughtException', console.error );
+
 ( new AppController() )
     .start( __dirname, process.cwd() )
     .then( ( success: boolean ) => {
