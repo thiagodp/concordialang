0cc80149128ec558e415d620c16d65fe973cddf2
Thiago Delgado Pinto <thiago-dp@bol.com.br>
2018-03-20 15:12:01 -0300

    Fixes NLP constant recognizer to accept 1 character

--- modules/nlp/NLP.ts
+++ modules/nlp/NLP.ts
@@ -317,7 +317,7 @@ class EntityRecognizerMaker {
      */
     public makeConstant( entityName: string ): any {
         var valueRec = new Bravey.RegexEntityRecognizer( entityName, 10 );
-        const regex = /\[[a-zA-ZÀ-ÖØ-öø-ÿ_][a-zA-ZÀ-ÖØ-öø-ÿ0-9 _-]+\]/g;
+        const regex = /\[[a-zA-ZÀ-ÖØ-öø-ÿ_][a-zA-ZÀ-ÖØ-öø-ÿ0-9 _-]*\]/g;
         valueRec.addMatch(
             regex,
             function( match ) {
