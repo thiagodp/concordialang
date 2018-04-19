a3f1648bca8c89f77796bfcbffad5a1a5039c20e
Thiago Delgado Pinto <thiago-dp@bol.com.br>
2018-04-18 21:03:32 -0300

    Changes escape to not escape single quotes and percent

--- modules/testdata/util/escape.ts
+++ modules/testdata/util/escape.ts
@@ -1,21 +1,23 @@
 //
 // Inspired in https://stackoverflow.com/questions/7744912/making-a-javascript-string-sql-friendly/7760578#7760578
 //
-
+// Adapted for Concordia needs.
+//
 
 function escapeChar( char ) {
     switch ( char ) {
+        // special
         case '\0':      return '\\0';
         case '\x08':    return '\\b';
         case '\x09':    return '\\t';
         case '\x1a':    return '\\z';
         case '\n':      return '\\n';
         case '\r':      return '\\r';
-        // Prepends a backslash to backslash, percent, and double/single quotes
+        // symbols
         case '\"': ;    // continue
-        case '\'': ;    // continue
-        case '\\': ;    // continue
-        case '%':       return '\\' + char;
+        // case '\'': ; // <-- do not escape single quote
+        // case '%': ;  // continue
+        case '\\':      return '\\' + char;
     }
     return char;
 }
