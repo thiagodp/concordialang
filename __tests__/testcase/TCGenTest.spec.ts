8bb97af9263ac69aed550c4bb54b98181185356e
Thiago Delgado Pinto <thiago-dp@bol.com.br>
2018-04-19 06:37:38 -0300

    Adds new test to TCGenTest

--- __tests__/testcase/TCGenTest.spec.ts
+++ __tests__/testcase/TCGenTest.spec.ts
@@ -16,6 +16,7 @@ import { IndexOfEachStrategy } from "../../modules/selection/CombinationStrategy
 import { Variant } from "../../modules/ast/Variant";
 import { TestScenario } from "../../modules/testscenario/TestScenario";
 import { LongLimits } from "../../modules/testdata/limits/LongLimits";
+import { TestCase } from "../../modules/ast/TestCase";
 
 describe( 'TCGenTest', () => {
 
@@ -92,7 +93,7 @@ describe( 'TCGenTest', () => {
         expect( errors ).toHaveLength( 0 );
         expect( testCases ).toHaveLength( 1 );
 
-        const tc = testCases[ 0 ];
+        const tc: TestCase = testCases[ 0 ];
 
         // Content + Comment
         const lines = tc.sentences.map( s => s.content + ( ! s.comment ? '' : ' #' + s.comment ) );
@@ -106,6 +107,73 @@ describe( 'TCGenTest', () => {
                 'Então, eu devo ver a mensagem "bar"' // << Then replaced with the oracle
             ]
         );
+
+        expect( tc.shoudFail ).toBeFalsy();
+
+    } );
+
+
+
+    it( 'indicates that should fail when no Otherwise is declared and has Then sentence withou state', async () => {
+
+        let spec = new Spec( '.' );
+
+        let doc1: Document = cp.addToSpec( spec,
+            [
+                '#language:pt',
+                'Feature: Feature 1',
+                'Scenario: Foo',
+                'Variant: Foo',
+                '  Quando eu preencho {A}',
+                '    E eu preencho <b> com "foo"',
+                ' Então eu devo ver "x"',
+                'Elemento de IU: A',
+                ' - valor mínimo é 5'
+            ],
+            { path: 'doc1.feature', hash: 'doc1' } as FileInfo
+        );
+
+        const specFilter = new SpecFilter( spec );
+        const batchSpecAnalyzer = new BatchSpecificationAnalyzer();
+        let errors: LocatedException[] = [],
+        warnings: LocatedException[] = [];
+
+        await batchSpecAnalyzer.analyze( specFilter.graph(), spec, errors );
+
+        // expect( doc1.fileErrors ).toEqual( [] );
+        // expect( doc2.fileErrors ).toEqual( [] );
+
+        const testPlanMakers: TestPlanMaker[] = [
+            // new TestPlanMaker( new AllValidMix(), new SingleRandomOfEachStrategy( SEED ) )
+            new TestPlanMaker( new JustOneInvalidMix(), new IndexOfEachStrategy( 0 ) )
+        ];
+
+        const ctx1 = new GenContext( spec, doc1, errors, warnings );
+        const variant1: Variant = doc1.feature.scenarios[ 0 ].variants[ 0 ];
+
+        let ts = new TestScenario();
+        ts.steps = variant1.sentences;
+
+        const testCases = await gen.generate( ts, ctx1, testPlanMakers );
+        expect( errors ).toHaveLength( 0 );
+        expect( testCases ).toHaveLength( 1 );
+
+        const tc: TestCase = testCases[ 0 ];
+
+        // Content + Comment
+        const lines = tc.sentences.map( s => s.content + ( ! s.comment ? '' : ' #' + s.comment ) );
+        const value1 = LongLimits.MIN;
+        const comment = '# inválido: menor valor aplicável';
+
+        expect( lines ).toEqual(
+            [
+                'Quando eu preencho <a> com "' + value1 + '" ' + comment,
+                'E eu preencho <b> com "foo"',
+                'Então eu devo ver "x"' // << same Then statement
+            ]
+        );
+
+        expect( tc.shoudFail ).toBeTruthy();
 
     } );
 
