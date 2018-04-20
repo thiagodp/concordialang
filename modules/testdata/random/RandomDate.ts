5b1fc471784949c5ad6681ba66340a059894727b
Thiago Delgado Pinto <thiago-dp@bol.com.br>
2018-04-20 16:24:42 -0300

    Fixes RandomDate

--- modules/testdata/random/RandomDate.ts
+++ modules/testdata/random/RandomDate.ts
@@ -1,10 +1,10 @@
 import { RandomLong } from "./RandomLong";
-import { LocalDate, Period } from "js-joda";
+import { LocalDate, ChronoUnit } from "js-joda";
 import { DateLimits } from "../limits/DateLimits";
 
 /**
  * Generates random date values.
- * 
+ *
  * @author Thiago Delgado Pinto
  */
 export class RandomDate {
@@ -14,12 +14,12 @@ export class RandomDate {
 
     /**
      * Returns a random date between two given dates, both inclusive.
-     * 
+     *
      * @param min Minimum date
      * @param max Maximum date
      */
     public between( min: LocalDate, max: LocalDate ): LocalDate {
-        const daysBetween: number = Period.between( min, max ).days();
+        const daysBetween: number = min.until( max, ChronoUnit.DAYS );
         if ( 0 === daysBetween ) {
             return min;
         }
@@ -29,7 +29,7 @@ export class RandomDate {
 
     /**
      * Returns a random date before the given date.
-     * 
+     *
      * @param max Maximum date
      */
     public before( max: LocalDate ): LocalDate {
@@ -38,11 +38,11 @@ export class RandomDate {
 
     /**
      * Returns a random date after the given date.
-     * 
+     *
      * @param min Minimum date
      */
     public after( min: LocalDate ): LocalDate {
         return this.between( min.plusDays( 1 ), DateLimits.MAX );
     }
-    
+
 }
