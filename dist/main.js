5dd17675686230b6f2636c64d677dbaa4fdaebb5
Thiago Delgado Pinto <thiago-dp@bol.com.br>
2018-06-11 13:08:24 -0300

    Change main to handle `SIGINT`

--- dist/main.js
+++ dist/main.js
@@ -2,6 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const AppController_1 = require("./modules/app/AppController");
 process.on('uncaughtException', console.error);
+process.on('SIGINT', () => {
+    console.log('\nAborted. Bye!');
+    process.exit(1);
+});
 (new AppController_1.AppController())
     .start(__dirname, process.cwd())
     .then((success) => {
--- main.ts
+++ main.ts
@@ -2,6 +2,11 @@ import { AppController } from "./modules/app/AppController";
 
 process.on( 'uncaughtException', console.error );
 
+process.on( 'SIGINT', () => {
+	console.log( '\nAborted. Bye!' );
+	process.exit( 1 );
+} );
+
 ( new AppController() )
     .start( __dirname, process.cwd() )
     .then( ( success: boolean ) => {
