7399e9a2c210e53c68cba3392b2fade6cf9f8cc3
Thiago Delgado Pinto <thiago-dp@bol.com.br>
2019-04-20 17:16:51 -0300

    Refactor and optimize training data

---
+++ modules/nlp/BaseTrainingExamples.ts
@@ -0,0 +1,164 @@
+import { NLPTrainingIntentExample } from "./NLPTrainingData";
+
+export const BASE_TRAINING_EXAMPLES: NLPTrainingIntentExample[] = [
+
+    {
+        "intent": "testcase",
+        "sentences": [
+
+            "{ui_action} {ui_element}",
+            "{ui_action} {ui_literal}",
+            "{ui_action} {value}",
+            "{ui_action} {number}",
+            "{ui_action} {constant}",
+            "{ui_action} {state}",
+
+            "{ui_action} {ui_element} {value}",
+            "{ui_action} {ui_element} {number}",
+            "{ui_action} {ui_element} {constant}",
+            "{ui_action} {ui_element} {ui_action_option}",
+            "{ui_action} {ui_element} {ui_action_option} {ui_element}",
+            "{ui_action} {ui_element} {ui_action_option} {ui_literal}",
+
+            "{ui_action} {value} {ui_element}",
+            "{ui_action} {number} {ui_element}",
+            "{ui_action} {constant} {ui_element}",
+
+            "{ui_action} {ui_literal} {value}",
+            "{ui_action} {ui_literal} {number}",
+            "{ui_action} {ui_literal} {constant}",
+            "{ui_action} {ui_literal} {ui_action_option}",
+            "{ui_action} {ui_literal} {ui_action_option} {ui_element}",
+            "{ui_action} {ui_literal} {ui_action_option} {ui_literal}",
+
+            "{ui_action} {value} {ui_literal}",
+            "{ui_action} {number} {ui_literal}",
+            "{ui_action} {constant} {ui_literal}",
+
+            "{ui_action} {value} {ui_literal} {ui_action_option}",
+            "{ui_action} {number} {ui_literal} {ui_action_option}",
+            "{ui_action} {constant} {ui_literal} {ui_action_option}",
+
+            "{ui_action} {ui_element_type} {value}",
+            "{ui_action} {ui_element_type} {number}",
+            "{ui_action} {ui_element_type} {constant}",
+
+            "{ui_action} {ui_element_type} {ui_property} {value}",
+            "{ui_action} {ui_element_type} {ui_property} {number}",
+            "{ui_action} {ui_element_type} {ui_property} {constant}",
+
+            "{ui_action} {value} {ui_element_type}",
+            "{ui_action} {number} {ui_element_type}",
+            "{ui_action} {constant} {ui_element_type}",
+
+            "{ui_action} {ui_element_type} {ui_element}",
+            "{ui_action} {ui_element_type} {ui_element} {value}",
+            "{ui_action} {ui_element_type} {ui_element} {number}",
+            "{ui_action} {ui_element_type} {ui_element} {constant}",
+
+            "{ui_action} {value} {ui_element_type} {ui_element}",
+            "{ui_action} {number} {ui_element_type} {ui_element}",
+            "{ui_action} {constant} {ui_element_type} {ui_element}",
+
+            "{ui_action} {ui_element_type} {ui_literal}",
+            "{ui_action} {ui_element_type} {ui_literal} {value}",
+            "{ui_action} {ui_element_type} {ui_literal} {number}",
+            "{ui_action} {ui_element_type} {ui_literal} {constant}",
+
+            "{ui_action} {value} {ui_element_type} {ui_literal}",
+            "{ui_action} {number} {ui_element_type} {ui_literal}",
+            "{ui_action} {constant} {ui_element_type} {ui_literal}",
+
+            "{ui_action_modifier} {ui_action} {ui_element}",
+            "{ui_action_modifier} {ui_action} {ui_element} {value}",
+            "{ui_action_modifier} {ui_action} {ui_element} {number}",
+            "{ui_action_modifier} {ui_action} {ui_element} {constant}",
+
+            "{ui_action_modifier} {ui_action} {value} {ui_element}",
+            "{ui_action_modifier} {ui_action} {number} {ui_element}",
+            "{ui_action_modifier} {ui_action} {constant} {ui_element}",
+
+            "{ui_action_modifier} {ui_action} {ui_literal}",
+            "{ui_action_modifier} {ui_action} {ui_literal} {value}",
+            "{ui_action_modifier} {ui_action} {ui_literal} {number}",
+            "{ui_action_modifier} {ui_action} {ui_literal} {constant}",
+
+            "{ui_action_modifier} {ui_action} {value} {ui_literal}",
+            "{ui_action_modifier} {ui_action} {number} {ui_literal}",
+            "{ui_action_modifier} {ui_action} {constant} {ui_literal}",
+
+            "{ui_action_modifier} {ui_action} {ui_element_type}",
+            "{ui_action_modifier} {ui_action} {ui_element_type} {value}",
+            "{ui_action_modifier} {ui_action} {ui_element_type} {number}",
+            "{ui_action_modifier} {ui_action} {ui_element_type} {constant}",
+
+            "{ui_action_modifier} {ui_action} {value} {ui_element_type}",
+            "{ui_action_modifier} {ui_action} {number} {ui_element_type}",
+            "{ui_action_modifier} {ui_action} {constant} {ui_element_type}",
+            "{ui_action_modifier} {ui_action} {ui_element_type} {ui_property} {value}",
+            "{ui_action_modifier} {ui_action} {ui_element_type} {ui_property} {number}",
+            "{ui_action_modifier} {ui_action} {ui_element_type} {ui_property} {constant}",
+
+            "{ui_action_modifier} {ui_action} {ui_action_option} {value}",
+            "{ui_action_modifier} {ui_action} {ui_action_option} {ui_action_option} {value}",
+
+            "{ui_action_modifier} {ui_action} {ui_element_type} {ui_action_option} {value}",
+            "{ui_action_modifier} {ui_action} {ui_element_type} {ui_action_option} {constant}",
+            "{ui_action_modifier} {ui_action} {ui_element_type} {ui_action_option} {number}",
+
+            "{ui_action_modifier} {ui_action} {value} {ui_action_option}",
+            "{ui_action_modifier} {ui_action} {number} {ui_action_option}",
+            "{ui_action_modifier} {ui_action} {constant} {ui_action_option}",
+
+            "{exec_action} {state}",
+
+            "{ui_action} {ui_action_option} {command}",
+
+            // "eu não vejo a url com {value}"
+        ]
+    },
+
+    {
+        "intent": "ui",
+        "sentences": [
+            "{ui_property} {ui_connector} {value}",
+            "{ui_property} {ui_connector} {number}",
+            "{ui_property} {ui_connector} {constant}",
+            "{ui_property} {ui_connector} {value_list}",
+            "{ui_property} {ui_connector} {ui_data_type}",
+            "{ui_property} {ui_connector} {ui_element_type}",
+            "{ui_property} {ui_connector} {query}",
+            // "id é {value}",
+            // "comprimento máximo é {number}",
+            // "valor vem da consulta {query}",
+            // "tipo é {ui_element_type}",
+            // "tipo é botão"
+        ]
+    },
+
+    {
+        "intent": "ui_item_query",
+        "sentences": [
+            "{ui_property} {query}",
+            // "{ui_property} vem de {query}",
+            // "{ui_property} vem da consulta {query}",
+            // "{ui_property} vem da query {query}",
+            // "{ui_property} está em {query}",
+            // "{ui_property} está contido em {query}",
+            // "{ui_property} está presente em {query}"
+        ]
+    },
+
+    {
+        "intent": "database",
+        "sentences": [
+            "{db_property} {ui_connector} {value}",
+            "{db_property} {ui_connector} {number}",
+            // "caminho é {value}",
+            // "nome é {value}",
+            // "tipo é {value}",
+            // "porta é {number}"
+        ]
+    }
+
+];
--- modules/nlp/NLPTrainer.ts
+++ modules/nlp/NLPTrainer.ts
@@ -1,9 +1,13 @@
+import * as deepcopy from 'deepcopy';
+
 import { LanguageContent } from '../dict/LanguageContent';
 import { LanguageContentLoader } from '../dict/LanguageContentLoader';
 import { isDefined } from '../util/TypeChecking';
+
 import { NLP } from './NLP';
-import { NLPTrainingData } from './NLPTrainingData';
+import { NLPTrainingData, NLPTrainingIntentExample } from './NLPTrainingData';
 import { NLPTrainingDataConversor } from './NLPTrainingDataConversor';
+import { BASE_TRAINING_EXAMPLES } from './BaseTrainingExamples';
 
 /**
  * NLP trainer.
@@ -12,8 +16,10 @@ import { NLPTrainingDataConversor } from './NLPTrainingDataConversor';
  */
 export class NLPTrainer {
 
-    private _convertedData: Map< string, NLPTrainingData > =
-        new Map< string, NLPTrainingData >();
+    private readonly LANGUAGE_INTENT_SEPARATOR = ':';
+    private readonly ALL_INTENTS = '*';
+
+    private _trainedIntents: string[] = [];
 
     constructor(
         private _langLoader: LanguageContentLoader
@@ -24,35 +30,96 @@ export class NLPTrainer {
         return this._langLoader.has( language );
     }
 
+    isIntentTrained( language: string, intent: string ): boolean {
+        return this._trainedIntents.indexOf( this.joinLanguageAndIntent( language, intent ) ) >= 0
+            || this._trainedIntents.indexOf( this.joinLanguageAndIntent( language, this.ALL_INTENTS ) ) >= 0;
+    }
+
+    private joinLanguageAndIntent( language: string, intent: string ): string  {
+        return  ( language + this.LANGUAGE_INTENT_SEPARATOR + ( intent || this.ALL_INTENTS ) )
+            .toLowerCase();
+    }
+
     trainNLP( nlp: NLP, language: string, intentNameFilter?: string ): boolean {
 
         if ( ! this.canBeTrained( language ) ) {
             return false;
         }
 
-        let data: NLPTrainingData = this._convertedData[ language ];
-        if ( ! data ) {
-            let content: LanguageContent = this._langLoader.load( language );
-
-            // Copy "ui_element_type" from "testcase" to "ui"
-            if ( isDefined( content.nlp[ "testcase" ] )
-                && isDefined( content.nlp[ "testcase" ][ "ui_element_type" ] )
-                && isDefined( content.nlp[ "ui" ]
-                && ! isDefined( content.nlp[ "ui" ][ "ui_element_type" ] )
-            )
-            ) {
-                content.nlp[ "ui" ][ "ui_element_type" ] = content.nlp[ "testcase" ][ "ui_element_type" ];
-            }
-
-
-            let conversor: NLPTrainingDataConversor = new NLPTrainingDataConversor();
-            data = conversor.convert( content.nlp || {}, content.training || [] );
-            this._convertedData[ language ] = data;
-        } else {
-            data = this._convertedData[ language ];
+        if ( this.isIntentTrained( language, intentNameFilter ) ) {
+            return true;
         }
 
-        nlp.train( language, data, intentNameFilter );
+        let content: LanguageContent = deepcopy( this._langLoader.load( language ) );
+
+        // Add base training sentences;
+
+        if ( isDefined( intentNameFilter ) ) {
+
+            // console.log( 'INTENT DEFINED:', intentNameFilter );
+
+            let example = content.training.find( ex => ex.intent === intentNameFilter );
+            const baseIntentExample = BASE_TRAINING_EXAMPLES.find( ex => ex.intent === intentNameFilter );
+
+            if ( isDefined( example ) && isDefined( baseIntentExample ) ) {
+
+                // console.log( intentNameFilter );
+                // console.log( 'BEFORE' );
+                // console.log( example.sentences.length );
+
+                // Concat with base training sentences
+                example.sentences = baseIntentExample.sentences.concat( example.sentences );
+
+                // console.log( 'AFTER' );
+                // console.log( example.sentences.length );
+            }
+
+        } else {
+
+            // console.log( 'INTENT NOT DEFINED' );
+
+            // Concat with base training sentences
+            for ( const baseEx of BASE_TRAINING_EXAMPLES ) {
+
+                if ( this.isIntentTrained( language, baseEx.intent ) ) {
+                    // console.log( '-> base intent already trained:', baseEx.intent );
+                    continue;
+                }
+                // console.log( '-> base intent untrained:', baseEx.intent );
+
+                let example = content.training.find( ex => ex.intent === baseEx.intent );
+                if ( ! example ) {
+                    continue;
+                }
+
+                // console.log( baseEx.intent );
+                // console.log( 'BEFORE' );
+                // console.log( example.sentences.length );
+
+                example.sentences = baseEx.sentences.concat( example.sentences );
+
+                // console.log( 'AFTER' );
+                // console.log( example.sentences.length );
+
+                this._trainedIntents.push( this.joinLanguageAndIntent( language, baseEx.intent ) );
+            }
+        }
+
+        // Copy "ui_element_type" from "testcase" to "ui"
+        if ( isDefined( content.nlp[ "testcase" ] )
+            && isDefined( content.nlp[ "testcase" ][ "ui_element_type" ] )
+            && isDefined( content.nlp[ "ui" ]
+            && ! isDefined( content.nlp[ "ui" ][ "ui_element_type" ] )
+        )
+        ) {
+            content.nlp[ "ui" ][ "ui_element_type" ] = content.nlp[ "testcase" ][ "ui_element_type" ];
+        }
+
+        let conversor: NLPTrainingDataConversor = new NLPTrainingDataConversor();
+        let converted: NLPTrainingData = conversor.convert( content.nlp || {}, content.training || [] );
+
+        nlp.train( language, converted, intentNameFilter );
+
         return true;
     }
 
