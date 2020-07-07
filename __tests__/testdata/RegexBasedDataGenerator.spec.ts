24e3929c1659485901a7a36c7ca271f1e3a5cb54
Thiago Delgado Pinto <thiago-dp@bol.com.br>
2020-07-07 00:08:44 -0300

    Add maximum string limit to regex values

--- __tests__/testdata/RegexBasedDataGenerator.spec.ts
+++ __tests__/testdata/RegexBasedDataGenerator.spec.ts
@@ -2,12 +2,23 @@ import { Random } from "../../modules/testdata/random/Random";
 import { RandomLong } from "../../modules/testdata/random/RandomLong";
 import { RandomString } from "../../modules/testdata/random/RandomString";
 import { RegexBasedDataGenerator } from "../../modules/testdata/RegexBasedDataGenerator";
+import { ValueType } from "../../modules/util";
 
 describe( 'RegexBaseDataGenerator', () => {
 
     const ran = new Random();
     const ranL = new RandomLong( ran );
-    const ranS = new RandomString( ran );
+	const ranS = new RandomString( ran );
+
+    const checkValid = ( exp ) => {
+        const gen = new RegexBasedDataGenerator( ranL, ranS, exp );
+        expect( gen.valid() ).toMatch( new RegExp( exp ) );
+    };
+
+    const checkInvalid = ( exp ) => {
+        const gen = new RegexBasedDataGenerator( ranL, ranS, exp );
+        expect( gen.invalid() ).not.toMatch( new RegExp( exp ) );
+    };
 
     it( 'valid values', () => {
         checkValid( '[a-z]' );
@@ -31,18 +42,25 @@ describe( 'RegexBaseDataGenerator', () => {
         checkInvalid( '[0-9]' );
         checkValid( '[A-z0-9 .-]{2,5}' );
         checkInvalid( '[0-9]{2}/[0-9]{2}/[0-9]{4}' );
-    } );
+	} );
 
+	it( 'limits to the given maximum', () => {
 
-    let checkValid = ( exp ) => {
-        let gen = new RegexBasedDataGenerator( ranL, ranS, exp );
-        expect( gen.valid() ).toMatch( new RegExp( exp ) );
-    };
+		const exp = '[a-z]';
+		const maxSize = 5;
+		const gen = new RegexBasedDataGenerator(
+			ranL, ranS, exp, ValueType.STRING, 10, maxSize );
 
-    let checkInvalid = ( exp ) => {
-        let gen = new RegexBasedDataGenerator( ranL, ranS, exp );
-        expect( gen.invalid() ).not.toMatch( new RegExp( exp ) );
-    };
+		for ( let i = 0; i < 10; ++i ) {
+			const validValue = gen.valid();
+			expect( validValue ).toMatch( new RegExp( exp ) );
+			expect( validValue.length ).toBeLessThanOrEqual( maxSize );
 
+			const invalidValue = gen.invalid();
+			expect( invalidValue ).not.toMatch( new RegExp( exp ) );
+			expect( invalidValue.length ).toBeLessThanOrEqual( maxSize );
+		}
+
+	} );
 
 } );
